afc87a8e87c11d744dfb31367fb3819d8986bd6c
rhx1138 <ryanjhendrix@gmail.com>
2022-11-15 18:26:18 -0500

    updated about.js

---
+++ src/About.js
@@ -0,0 +1,31 @@
+function About () {
+    return (
+        <>
+        <div className="">
+        <div className="px-10">
+          <h1 className="text-3xl">Developer and designer</h1>
+          <p className="text-gray-600">Freelancer providing services for web development and design content needs.<br></br>
+          Please see my work below.</p>
+        </div>
+        <div className="mx-auto bg-gradient-to-b from-cyan-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <img src={`${process.env.PUBLIC_URL}/propic1bg.png`} layout="fill" objectFit="cover" />
+        </div>
+        </div>
+        <div>
+        <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+        <p className="text-lg py-2 leading-8">
+          Since the beginning of my journey as a freelance designer and
+          developer, I've done remote work for agencies
+          and collaborated with talanted people to create digital products
+          for both business and consumer use.
+        </p>
+        <p className="text-lg py-2 leading-8">
+          I offer from a wide range of services, including brand design,
+          web design and development, and content creation.
+        </p>
+      </div>
+      </>
+    ) 
+}
+
+export default About
